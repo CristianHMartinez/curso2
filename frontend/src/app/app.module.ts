72d1f19e39b367434d6b6e0feb7d95483f3ee53d
CristianHMartinez <cristian.martinez.716cm9@gmail.com>
2026-09-17 22:20:19 +0000

    Add AvisoNuevo component for creating new notifications and integrate it into the app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FormsModule } from '@angular/forms';
 import { EntrarComponent } from './entrar/entrar.component';
 import { AuthInterceptor } from './interceptores/auth.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AvisoNuevoComponent } from './aviso-nuevo/aviso-nuevo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AvisosListaComponent,
-    EntrarComponent
+    EntrarComponent,
+    AvisoNuevoComponent
   ],
   imports: [
     BrowserModule,
