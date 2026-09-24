a5d5179b4fe616ce64c392dc5180a8dda7a22cac
CristianHMartinez <cristian.martinez.716cm9@gmail.com>
2026-09-24 22:34:23 +0000

    Add models, serializers, views, and permissions for Aviso and Categoria; configure API endpoints and routing

--- api-django/avisos/views.py
+++ api-django/avisos/views.py
@@ -1,3 +1,23 @@
 from rest_framework import permissions, viewsets
 
-# Aqui escribes AvisoViewSet y la vista yo (guia 02, pasos 2 y 4).
+from .models import Aviso
+from .serializers import AvisoSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+
+class AvisoViewSet(viewsets.ModelViewSet):
+    queryset = Aviso.objects.filter(publicado=True).select_related("categoria", "autor")
+    serializer_class = AvisoSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(autor=self.request.user)
+
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticatedOrReadOnly, EsAutorOAdmin])
+def yo(request):
+    return Response({
+        "id": request.user.id,
+        "nombre": request.user.username,
+        "rol": "admin" if request.user.is_staff else "autor",
+    })
